e1f5889871d3d6d102eda9ad4521e073e7d3d0ae
Artem Fedorov <artemka.fedorov@gmail.com>
2023-12-23 23:20:02 +0000

    [Driver] add new mongo requests

--- projects/driver/internal/service/adapters/repository.go
+++ projects/driver/internal/service/adapters/repository.go
@@ -7,7 +7,9 @@ import (
 )
 
 type DriverRepository interface {
-	GetTripByID(ctx context.Context, tripId uuid.UUID) (domain.Trip, error)
+	GetTripsByStatus(ctx context.Context, status domain.TripStatus) ([]domain.Trip, error)
+	GetTripByID(ctx context.Context, tripId uuid.UUID) (*domain.Trip, error)
+	UpdateTrip(ctx context.Context, tripId uuid.UUID, updatedTrip domain.Trip) error
 	ChangeTripStatus(ctx context.Context, tripId uuid.UUID, status domain.TripStatus) error
-	CreateTrip(ctx context.Context, status domain.Trip) error
+	InsertTrip(ctx context.Context, status domain.Trip) error
 }
